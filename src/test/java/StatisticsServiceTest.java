f536af804ebafb4f1da1a384e8efe14e373c55e0
KlimovaTE <t_klimova@inbox.ru>
2022-08-02 01:58:24 +0300

    add CheckStyle

---
+++ src/test/java/StatisticsServiceTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatisticsServiceTest {
+
+    @Test
+    void findMaxIfMaxFirst() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxIfMaxNotFirst() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 26, 11, 11, 12};
+        long expected = 26;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+}
